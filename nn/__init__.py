05fc9c53147d30e7453f0e6d5685262a4c8c2990
zoechen0717 <zoechen0717@gmail.com>
2025-03-18 13:49:51 -0700

    test

--- nn/__init__.py
+++ nn/__init__.py
@@ -2,5 +2,6 @@
 BMI203: Biocomputing Algorithms - Winter 2023
 Final project: neural networks
 """
-
+from .nn import NeuralNetwork
+from .nn import binary_cross_entropy, binary_cross_entropy_backprop, mean_squared_error, mean_squared_error_backprop
 __version__ = "0.1.0"
